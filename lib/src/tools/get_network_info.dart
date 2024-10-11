82205a05663cb516a76b576c1db02d3a6bc6ab0b
Gerardo Villegas <villegasgerardo.j@gmail.com>
2024-10-11 08:09:26 -0500

    chore: add ip info on network info tool

--- lib/src/tools/get_network_info.dart
+++ lib/src/tools/get_network_info.dart
@@ -1,12 +1,21 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:network_info_plus/network_info_plus.dart';
 
 Future<String> getNetworkType() async {
   final connectivityResult = await Connectivity().checkConnectivity();
+  final networkInfo = NetworkInfo();
+  final wifiIPv4Addr = await networkInfo.getWifiIP();
 
   if (connectivityResult == ConnectivityResult.mobile) {
-    return 'Connected by mobile data';
+    return '''
+        Connected by: Mobile data <br>
+        IP: $wifiIPv4Addr
+      ''';
   } else if (connectivityResult == ConnectivityResult.wifi) {
-    return 'Connected by Wi-Fi';
+    return '''
+        Connected by: Wi-Fi <br>
+        IP: $wifiIPv4Addr
+      ''';
   } else {
     return 'Not internet connection';
   }
